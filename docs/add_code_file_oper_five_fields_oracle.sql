5b7d7b9cb02fe01b0c001ff0ce4acbeac6e406c6
tangguo <tangguo@tydic.com>
2026-04-14 17:08:13 +0800

    refactor: 将 Oracle SQL 改为 MySQL 语法
    - 修改 add_code_file_oper_five_fields_oracle.sql
    - 修改 add_code_file_oper_row_options.sql
    - NUMBER(1) -> INT, CLOB -> TEXT
    - 调整注释语法为 MySQL 格式

--- docs/add_code_file_oper_five_fields_oracle.sql
+++ docs/add_code_file_oper_five_fields_oracle.sql
@@ -1,17 +1,9 @@
--- CODE_FILE_OPER 表增加 5 个字段（Oracle）
+-- CODE_FILE_OPER 表增加 5 个字段（MySQL）
 -- 1) file_list_shell 开关  2) 上传/下载前 Groovy  3) 前 Groovy 开关
 -- 4) 上传/下载成功后 Groovy  5) 后 Groovy 开关
 
-ALTER TABLE CODE_FILE_OPER ADD (
-  FILE_LIST_SHELL_ENABLE       NUMBER(1) DEFAULT 1 NOT NULL,
-  FILE_OPER_GROOVY_BEFORE      CLOB,
-  FILE_OPER_GROOVY_BEFORE_ENABLE NUMBER(1) DEFAULT 0 NOT NULL,
-  FILE_OPER_GROOVY_AFTER       CLOB,
-  FILE_OPER_GROOVY_AFTER_ENABLE NUMBER(1) DEFAULT 0 NOT NULL
-);
-
-COMMENT ON COLUMN CODE_FILE_OPER.FILE_LIST_SHELL_ENABLE IS '是否执行 FILE_LIST_SHELL：0 否 1 是（为 1 且 FILE_LIST_SHELL 非空时走 Shell 列文件）';
-COMMENT ON COLUMN CODE_FILE_OPER.FILE_OPER_GROOVY_BEFORE IS 'SFTP 上传/下载前执行的 Groovy 脚本';
-COMMENT ON COLUMN CODE_FILE_OPER.FILE_OPER_GROOVY_BEFORE_ENABLE IS '是否执行前置 Groovy：0 否 1 是';
-COMMENT ON COLUMN CODE_FILE_OPER.FILE_OPER_GROOVY_AFTER IS 'SFTP 上传/下载成功后执行的 Groovy 脚本';
-COMMENT ON COLUMN CODE_FILE_OPER.FILE_OPER_GROOVY_AFTER_ENABLE IS '是否执行后置 Groovy：0 否 1 是';
+ALTER TABLE code_file_oper ADD COLUMN file_list_shell_enable INT DEFAULT 1 NOT NULL COMMENT '是否执行 FILE_LIST_SHELL：0 否 1 是（为 1 且 FILE_LIST_SHELL 非空时走 Shell 列文件）';
+ALTER TABLE code_file_oper ADD COLUMN file_oper_groovy_before TEXT COMMENT 'SFTP 上传/下载前执行的 Groovy 脚本';
+ALTER TABLE code_file_oper ADD COLUMN file_oper_groovy_before_enable INT DEFAULT 0 NOT NULL COMMENT '是否执行前置 Groovy：0 否 1 是';
+ALTER TABLE code_file_oper ADD COLUMN file_oper_groovy_after TEXT COMMENT 'SFTP 上传/下载成功后执行的 Groovy 脚本';
+ALTER TABLE code_file_oper ADD COLUMN file_oper_groovy_after_enable INT DEFAULT 0 NOT NULL COMMENT '是否执行后置 Groovy：0 否 1 是';
